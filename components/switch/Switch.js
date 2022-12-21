dc0a05e1bc7f7973efd6c101b6283d5c53b7faf8
Jacob Hildebrandt <jacob@commerce7.com>
2022-12-21 14:16:26 -0800

    try with real value

--- components/switch/Switch.js
+++ components/switch/Switch.js
@@ -6,7 +6,7 @@ const Switch = () => {
   const [checked, setChecked] = useState(false);
 
   useEffect(() => {
-    axios.put(process.env.NEXT_PUBLIC_PI_ENDPOINT, {
+    axios.put("https://f85f-2001-569-f805-fe00-bde5-1a61-c2f-27f4.ngrok.io", {
       enable: checked ? 0 : 1,
     });
   }, [checked]);
